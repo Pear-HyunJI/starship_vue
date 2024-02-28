d10401bbd9965c7c8829cf380c35802382cf611c
oktobat <chamsogum@gmail.com>
2024-02-28 18:15:41 +0900

    2/28일 8교시

--- src/components/section/SliderSection.vue
+++ src/components/section/SliderSection.vue
@@ -1,11 +1,14 @@
 <template>
     <section id="sliderSection">
         <div class="slide__inner">
-            <Swiper :options="swiperOption" class="swiper">
-                <SwiperSlide v-for="(slider, index) in sliders" :key="index">
+            <swiper class="swiper" :options="swiperOption">
+                <swiper-slide v-for="(slider, index) in sliders" :key="index">
                     <img :src="slider.image" :alt="slider.alt">
-                </SwiperSlide>
-            </Swiper>
+                </swiper-slide>
+                <div class="swiper-pagination" slot="pagination"></div>
+                <div class="swiper-button-prev" slot="button-prev"></div>
+                <div class="swiper-button-next" slot="button-next"></div>
+            </swiper>
         </div>
     </section>
 </template>
@@ -28,26 +31,30 @@
                     { image:"./image/main_slide_jsw_231212_pc.jpg", alt:"이미지3" }
                 ],
                 swiperOption:{
-                    slidesPerView : 1,
-                    // spaceBetween : 30,
+                    slidePerView:1,
+                    spaceBetween:0,
                     autoplay:{
-                       delay:1000
+                        delay : 1000
                     },
-                    loop : true,
-                    pagination:{
-                        el: '.swiper-pagination',
-                        clickable : true
-                    },
-                    navigation : {
-                        nextEl : '.swiper-button-next',
-                        prevEl : '.swiper-button-prev'
-                    }
+                    loop:true,
+                    pagination: { 
+                        el: '.swiper-pagination', 
+                        clickable: true 
+                    }, 
+                    navigation: { 
+                        nextEl: '.swiper-button-next', 
+                        prevEl: '.swiper-button-prev' 
+                    } 
                 }
             }
         },
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style scoped>
+    .swiper-button-prev,
+    .swiper-button-next {
+        color:red;
+    }
+    
 </style>
